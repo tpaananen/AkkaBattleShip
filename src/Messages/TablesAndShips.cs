cbb6df1c1f3c75bd5a4799ffa58c2098a4957a42
tpaananen <tommi.paananen@outlook.com>
2015-07-13 20:10:04 +0300

    ...

--- src/Messages/TablesAndShips.cs
+++ src/Messages/TablesAndShips.cs
@@ -10,10 +10,10 @@ namespace Messages.CSharp
         public static readonly IReadOnlyCollection<Tuple<string, int>> Ships
             = new ReadOnlyCollection<Tuple<string, int>>(new List<Tuple<string, int>>
             {
-                //Tuple.Create("Carrier", 5),
-                //Tuple.Create("BattleShip", 4),
-                //Tuple.Create("Cruiser", 3),
-                //Tuple.Create("Cruiser", 3),
+                Tuple.Create("Carrier", 5),
+                Tuple.Create("BattleShip", 4),
+                Tuple.Create("Cruiser", 3),
+                Tuple.Create("Cruiser", 3),
                 Tuple.Create("Destroyer", 2),
                 Tuple.Create("Submarine", 1)
             });
